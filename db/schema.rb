8996820b6904d10d9d95686965f753aa350a86d9
lisa.shiro <fortechacademy.risako1011@gmail.com>
2021-01-19 08:02:02 +0000

    fix_migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_131314) do
+ActiveRecord::Schema.define(version: 2021_01_17_110819) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
