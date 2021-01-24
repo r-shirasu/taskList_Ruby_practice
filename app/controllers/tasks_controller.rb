66d25c245360a5bbf76d68f8e764233a7a144146
lisa.shiro <fortechacademy.risako1011@gmail.com>
2021-01-24 12:45:26 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,5 @@
-
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in
   
   def index
     @tasks = Task.all
