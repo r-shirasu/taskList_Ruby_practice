2de12dc82fadc28c5e681b57b2bb1927ada8b01c
lisa.shiro <fortechacademy.risako1011@gmail.com>
2021-01-26 11:52:14 +0000

    set preparation_etc

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show,]
+  before_action :correct_user, only: [:destroy, :show]
   
   def index
     @tasks = Task.all
@@ -15,35 +16,36 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
     flash[:success] = 'Task が正常に投稿されました'
     redirect_to @task
     else
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     flash.now[:danger] = 'Task が投稿されませんでした'
     render :new
     end
   end
   
   def edit
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.build(task_params)
   end
   
   def update
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.build(task_params)
     
     if @task.update(task_params)
     flash[:success] = 'Task は正常に更新されました'
     redirect_to @task
     else
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     flash.now[:danger] = 'Task は更新されませんでした'
     render :edit
     end
   end
   
   def destroy
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.build(task_params)
     @task.destroy
     
     flash[:success] = 'Task は正常に削除されました'
@@ -58,11 +60,11 @@ class TasksController < ApplicationController
   end
   
   def task_params
-  params.require(:task).permit(:content, :status)
+  params.permit(:content, :status)
   end
   
   def correct_user
-    @task = current_user.microposts.find_by(id: params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
     unless @task
       redirect_to root_url
     end
