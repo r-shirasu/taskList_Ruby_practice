9840f3e1d7d28f8c59e8ab028e449bf76c0994c6
lisa.shiro <fortechacademy.risako1011@gmail.com>
2021-01-29 13:08:36 +0000

    not_login_remake

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :create, :show]
+  before_action :require_user_logged_in, only: [:index, :create, :destroy, :edit, :update, :show]
   before_action :correct_user, only: [:destroy, :edit, :update, :show]
   
   def index
