d31f7902711766d6e604d9b26ef91ce968d5ea1a
lisa.shiro <fortechacademy.risako1011@gmail.com>
2021-01-29 12:26:27 +0000

    remake_login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,29 @@
-module SessionsHelper
+#module SessionsHelper
   #渡されたユーザーでログイン
-  def log_in(user)
-    session[:user_id] = user.id
-  end
+  #def log_in(user)
+    #session[:user_id] = user.id
+  #end
   #ログイン中のユーザーを返す
+  #def current_user
+    #@current_user ||= User.find_by(id: session[:user_id])
+  #end
+
+  #def logged_in?
+   # !current_user.nil?
+  #end
+  
+  #def log_out
+   # session.delete(:user_id)
+    #@current_user = nil
+  #end
+#end
+
+module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
-    !current_user.nil?
-  end
-  
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
+    !!current_user
   end
 end
