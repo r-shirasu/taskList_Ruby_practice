bed2cf566f5a25ea5542fdcebd728c4dce31a577
lisa.shiro <fortechacademy.risako1011@gmail.com>
2021-01-30 11:44:21 +0000

    after_reviewed_14.1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -63,8 +63,3 @@ class TasksController < ApplicationController
 end
 
 
-  def show
-    @user = User.find(params[:id])
-    @microposts = @user.microposts.order(id: :desc).page(params[:page])
-    counts(@user)
-  end
